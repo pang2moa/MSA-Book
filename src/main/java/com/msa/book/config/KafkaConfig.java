36596a1db89265d6d586b35d8cf3848aaf33e545
pangmoa <vkdahdk@gmail.com>
2024-07-30 20:08:03 +0900

    kafka 수정

--- src/main/java/com/msa/book/config/KafkaConfig.java
+++ src/main/java/com/msa/book/config/KafkaConfig.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.KafkaTemplate; 
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
